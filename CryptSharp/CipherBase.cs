3e5cab801a0414e21a9a997993e9eea063b6cf8f
CryptoTrick <seanfife@hotmail.com>
2018-08-12 15:29:47 -0700

    New random alphabet function

--- CryptSharp/CipherBase.cs
+++ CryptSharp/CipherBase.cs
@@ -47,7 +47,7 @@ namespace CryptSharp.Ciphers
         {
             List<T> toEncrypt = new List<T>();
 
-            T[] generated = Utility.Random<T>(1024);
+            T[] generated = Utility.Random<T>(alphabet, 1024);
             int i = length;
 
             foreach (T c in generated)
