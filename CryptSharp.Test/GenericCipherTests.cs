4644b6e3c02fdd445b197a8c2a54cd93c3d03af4
CryptoTrick <seanfife@hotmail.com>
2018-08-12 19:00:48 -0700

    Adding in generic block cipher

---
+++ CryptSharp.Test/GenericCipherTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptSharp.Ciphers.Generic;
+
+namespace CryptSharp.Test
+{
+    [TestClass]
+    public class GenericCipherTests
+    {
+        [TestMethod]
+        public void Generic_GenericBlock()
+        {
+            GenericBlockCipher gbc = new GenericBlockCipher();
+            gbc.DiffuseFunction = diff;
+            gbc.ConfuseFunction = conf;
+
+            gbc.InverseDiffuseFunction = diff;
+            gbc.InverseConfuseFunction = conf;
+
+            gbc.Key = BitConverter.GetBytes(0x133457799BBCDFF1);
+            byte[] clear = BitConverter.GetBytes(0x0123456789ABCDEF);
+            gbc.BlockSize = gbc.Key.Length * 8;//this is to get this working without padding
+
+            byte[] cipher = gbc.Encrypt(clear);
+
+            CollectionAssert.AreNotEqual(clear, cipher);
+
+            byte[] plain = gbc.Decrypt(cipher);
+
+            CollectionAssert.AreEqual(clear, plain);
+        }
+
+        public byte[] diff(byte[] block, GenericBlockCipher cipher)
+        {
+            for(int j = 0; j < block.Length; j++)
+            {
+                block[j] = (byte)(block[j] ^ cipher.Key[j % cipher.Key.Length]);
+            }
+
+            return block;
+        }
+
+        public byte[] conf(byte[] block, GenericBlockCipher cipher)
+        {
+            Array.Reverse(block); 
+
+            return block;
+        }
+
+    }
+}
